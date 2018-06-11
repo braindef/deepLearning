87c4bdb2f16710bb845a056dac2a1ba578d8467c
Marc jun Landolt <mail@marclandolt.ch>
2018-06-11 07:15:38 +0200

    added gentoo install

--- shell/install.sh
+++ shell/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install python-pip
+
 pip install cython
 pip install word2vec
 pip install gensim
@@ -10,7 +11,10 @@ pip install word2vec --upgrade
 pip install gensim --upgrade
 
 
+
+
 sudo emerge dev-python/pip
+
 pip install cython --user
 pip install word2vec --user
 pip install gensim --user
@@ -18,3 +22,4 @@ pip install gensim --user
 pip install cython --user --upgrade
 pip install word2vec --user --upgrade
 pip install gensim --user --upgrade
+
